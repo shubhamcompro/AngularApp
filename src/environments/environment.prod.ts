883645a260b4c21ee0598f34f9098ba0c761cf84
Shubham Aggarwal <shubham.aggarwal@comprotechnologies.com>
2018-09-11 12:31:29 +0530

    add environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC8CO4oG1-xJOYCgQXDBusZQTdbcNYD9Lc',
+    authDomain: 'flipkart-8586e.firebaseapp.com',
+    databaseURL: 'https://flipkart-8586e.firebaseio.com',
+    projectId: 'flipkart-8586e',
+    storageBucket: 'flipkart-8586e.appspot.com',
+    messagingSenderId: '368962301023'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC8CO4oG1-xJOYCgQXDBusZQTdbcNYD9Lc',
+    authDomain: 'flipkart-8586e.firebaseapp.com',
+    databaseURL: 'https://flipkart-8586e.firebaseio.com',
+    projectId: 'flipkart-8586e',
+    storageBucket: 'flipkart-8586e.appspot.com',
+    messagingSenderId: '368962301023'
+  }
 };
 
 /*
