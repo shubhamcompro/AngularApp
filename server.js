f56e9e7bd24413e208dd80da09c34f8017416eaa
Shubham Aggarwal <shubham.aggarwal@comprotechnologies.com>
2018-09-11 12:19:32 +0530

    set heroku deploy config

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+
+app.get("/*", function (req, res) {
+  res.sendfile(path.join(__dirname + '/dist/index.html'))
+});
+
+console.log('App Started');
+
